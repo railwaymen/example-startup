ff34bf2691443794eb9358c64c7f2228b786127b
Grzegorz Forysinski <grzesiek@railwaymen.org>
2012-12-03 01:38:21 +0100

    step 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,18 @@
 class User < ActiveRecord::Base
+
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    unless user
+      user = User.create(name:auth.extra.raw_info.name,
+                         provider:auth.provider,
+                         uid:auth.uid,
+                         email:auth.info.email,
+                         password:Devise.friendly_token[0,20]
+                         )
+    end
+    user
+  end
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ExampleStartup::Application.routes.draw do
-  get "home/index"
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
-  devise_for :users
+  get "home/index"
 
   root :to => 'home#index'
 
